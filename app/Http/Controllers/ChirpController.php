d3c848c9abe4e6cc91eff17638e30e876a295ecd
Khant Sithu <khantseithuu@gmail.com>
2023-07-08 15:44:20 +0630

    feat: edit chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -47,8 +47,12 @@ class ChirpController extends Controller {
 	/**
 	 * Show the form for editing the specified resource.
 	 */
-	public function edit(Chirp $chirp) {
-		//
+	public function edit(Chirp $chirp): View{
+		$this->authorize('update', $chirp);
+
+		return view('chirps.edit', [
+			'chirp' => $chirp,
+		]);
 	}
 
 	/**
@@ -56,6 +60,15 @@ class ChirpController extends Controller {
 	 */
 	public function update(Request $request, Chirp $chirp) {
 		//
+		$this->authorize('update', $chirp);
+
+		$validated = $request->validate([
+			'message' => ['required', 'string', 'max:255'],
+		]);
+
+		$chirp->update($validated);
+
+		return redirect()->route('chirps.index');
 	}
 
 	/**
