2a96fb42a05052c4cb27ee6d077b661eee4f59db
Khant Sithu <khantseithuu@gmail.com>
2023-07-08 21:04:36 +0630

    feat: mail to users new post noti

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +10,10 @@ class Chirp extends Model {
 
 	protected $fillable = [
 		'message',
+	];
+
+	protected $dispatchesEvents = [
+		'created' => ChirpCreated::class,
 	];
 
 	public function user() {
